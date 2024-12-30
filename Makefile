bd90c05f4e86165d28b0d49800cf353844a08040
Rob-Ot <elkhailiissam0@gmail.com>
2024-12-30 00:26:50 +0000

    ft_printf 42 cursus project

---
+++ Makefile
@@ -0,0 +1,34 @@
+NAME = libftprintf.a
+CC = cc
+CFLAGS = -Wall -Wextra -Werror
+AR = ar rcs
+RM = rm -f
+
+SRCS = ft_printf.c \
+       ft_putstr.c \
+       ft_putchar.c \
+       ft_putnbr.c \
+       takeu.c \
+       takep.c \
+       takex.c \
+       takeux.c
+
+OBJS = $(SRCS:.c=.o)
+
+all: $(NAME)
+
+$(NAME): $(OBJS)
+	$(AR) $(NAME) $(OBJS)
+
+%.o: %.c
+	$(CC) $(CFLAGS) -c $< -o $@
+
+clean:
+	$(RM) $(OBJS)
+
+fclean: clean
+	$(RM) $(NAME)
+
+re: fclean all
+
+.PHONY: all clean fclean re
---
+++ main.c
@@ -0,0 +1,9 @@
+#include "printf.h"
+
+int main()
+{
+	printf("|------------------------------------------------------------ Printf Test --------------------------------------------------------------------|\n\n");
+
+	ft_printf("hello my name is %s and i am %d years old no i mean %u years old how ever i live in %p and my encrypted name is %x and %X \n \n \n","issam",15,19,NULL,255,255);
+	printf("hello my name is %s and i am %d years old no i mean %u years old how ever i live in %p and my encrypted name is %x and %X \n \n \n","issam",15,19,NULL,255,255);
+}
